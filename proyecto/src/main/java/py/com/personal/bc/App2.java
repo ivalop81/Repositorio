3fa75ba2d2eb320302a84b1be4392f9344cda8f5
Ivan Lopez <ivalop81@gmail.com>
2016-08-29 10:56:04 -0400

    Se agrego una nueva clase add2.java

---
+++ proyecto/src/main/java/py/com/personal/bc/App2.java
@@ -0,0 +1,13 @@
+package py.com.personal.bc;
+
+/**
+ * Hello world! Albert2
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
